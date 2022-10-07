7812a8eb9fe317384b33c2d77cbb5896841141ef
plutopeas <96548350+plutopeas@users.noreply.github.com>
2022-10-07 15:32:20 -0400

    Add files via upload

---
+++ Get365DistributionGroupMembers.ps1
@@ -0,0 +1,12 @@
+﻿$ExportPath = "C:\Users\mariahr.GO-DOMAIN\OneDrive - Integris\Documents"
+$Groups = Get-DistributionGroup -ResultSize Unlimited
+$Groups | ForEach-Object {
+$group = $_
+Get-DistributionGroupMember -Identity $group.Name -ResultSize Unlimited | ForEach-Object {
+      New-Object -TypeName PSObject -Property @{
+       Group = $group.DisplayName
+       Member = $_.Name
+       EmailAddress = $_.PrimarySMTPAddress
+       RecipientType= $_.RecipientType
+}}} | Export-CSV "$ExportPath\DistributionGroups.csv" -NoTypeInformation -Encoding UTF8
+Invoke-Item "$ExportPath\DistributionGroups.csv"

b38ae84325a68178d5178c46b0acad1e1e3eb346
plutopeas <96548350+plutopeas@users.noreply.github.com>
2022-10-07 15:36:39 -0400

    Add files via upload

---
+++ Get365GroupMembers.ps1
@@ -0,0 +1,13 @@
+﻿$ExportPath = "C:\Users\mariahr.GO-DOMAIN\OneDrive - Integris\Documents"
+$ExportName = "Office365GroupMembers.csv"
+$Groups = Get-UnifiedGroup -ResultSize Unlimited
+$Groups | ForEach-Object {
+$group = $_
+Get-UnifiedGroupLinks -Identity $group.Name -LinkType Members -ResultSize Unlimited | ForEach-Object {
+      New-Object -TypeName PSObject -Property @{
+       Group = $group.DisplayName
+       Member = $_.Name
+       EmailAddress = $_.PrimarySMTPAddress
+       RecipientType= $_.RecipientType
+}}} | Export-CSV "$ExportPath\$ExportName" -NoTypeInformation -Encoding UTF8
+Invoke-Item "$ExportPath\$ExportName"
